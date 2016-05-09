bffb95144c3dbe7a5e5637b0f31c733453638822
Shrimadhav U K <spechide@shrimadhavuk.me>
2016-05-09 18:44:17 +0530

    bootstarping all requirements

---
+++ js/functions.js
@@ -0,0 +1,17 @@
+var a =
+`
+ .d8888b.  888                       888
+d88P  Y88b 888                       888
+Y88b.      888                       888    This is a browser feature intended for
+ "Y888b.   888888  .d88b.  88888b.   888    developers. If someone told you to copy
+    "Y88b. 888    d88""88b 888 "88b  888    and paste something here to enable a
+      "888 888    888  888 888  888  Y8P    Facebook feature or "hack" someone's
+Y88b  d88P Y88b.  Y88..88P 888 d88P         account, it is a scam and will give them
+ "Y8888P"   "Y888  "Y88P"  88888P"   888    access to your Facebook account.
+                           888
+                           888
+                           888
+
+See https://www.facebook.com/selfxss for more information.
+`;
+console.log(a);
