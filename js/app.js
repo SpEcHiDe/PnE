ba4663762bce23c14d52ef059e801bf98d4a8956
Shrimadhav U K <spechide@shrimadhavuk.me>
2016-05-10 10:58:49 +0530

    tmp commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,58 @@
-var app = angular.module('keito', []);
+var app = angular.module('keito', [/*'ngRoute', 'ngSanitize', 'ui.select'*/]);
+
+/*
+app.config(function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl : 'assets/partials/home.html',
+			controller  : 'homeController'
+		})
+		.when('/events', {
+			templateUrl : 'assets/partials/events.html',
+			controller  : 'eventsController'
+		})
+		.when('/events/:eventCode', {
+			templateUrl : 'assets/partials/event.html',
+			controller  : 'eventController'
+		})
+		.when('/workshops', {
+			templateUrl : 'assets/partials/workshops.html',
+			controller  : 'workshopsController'
+		})
+		.when('/workshops/:eventCode', {
+			templateUrl : 'assets/partials/workshop.html',
+			controller  : 'eventController'
+		})
+		.when('/contact', {
+			templateUrl : 'assets/partials/contact.html',
+			controller  : 'contactController'
+		})
+		.when('/prodezza', {
+			templateUrl : 'assets/partials/prodezza.html',
+			controller  : 'prodezzaController'
+		})
+		.when('/proshows', {
+			templateUrl : 'assets/partials/proshows.html',
+			controller  : 'proshowsController'
+		})
+		.when('/sneharagam', {
+			templateUrl : 'assets/partials/sneharagam.html',
+			controller  : 'sneharagamController'
+		})
+		.when('/iink', {
+			templateUrl : 'assets/partials/iink.html',
+			controller  : 'iinkController'
+		})
+		.when('/sponsors', {
+			templateUrl : 'assets/partials/sponsor.html',
+			controller  : 'sponsorController'
+		})
+		.when('/_=_', {
+			templateUrl : 'assets/partials/home.html',
+			controller  : 'homeController'
+		})
+		.otherwise({
+	        redirectTo: '/'
+	    });
+});
+*/
