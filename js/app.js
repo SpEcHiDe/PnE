b1eeb50ebf19faf99ffc42e6ee09f8b4f2cc4eaf
Shrimadhav U K <spechide@shrimadhavuk.me>
2016-05-12 20:03:24 +0530

    invalid mistake

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 var app = angular.module('keito', ['ngRoute']);
 
+// create the controller and inject Angular's $scope
+app.controller('mainController', function($scope) {
+
+		// create a message to display in our view
+		$scope.message = 'Everyone come and see how good I look!';
+});
+
 		// configure our routes
     app.config(function($routeProvider, $locationProvider) {
         $routeProvider
