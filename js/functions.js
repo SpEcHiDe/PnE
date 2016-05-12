741f9e6573c6b9714ab3d9f3e926e8a5d3208904
Shrimadhav U K <spechide@shrimadhavuk.me>
2016-05-12 18:54:02 +0530

    hate mail

--- js/functions.js
+++ js/functions.js
@@ -1,21 +1,3 @@
-
-$('.carousel').carousel({
-  interval: 3500, // in milliseconds
-  pause: 'none' // set to 'true' to pause slider on mouse hover
-})
-
-$( "a.submenu" ).click(function() {
-$( ".menuBar" ).slideToggle( "normal", function() {
-// Animation complete.
-});
-});
-$( "ul li.dropdown a" ).click(function() {
-$( "ul li.dropdown ul" ).slideToggle( "normal", function() {
-// Animation complete.
-});
-$('ul li.dropdown').toggleClass('current');
-});
-
 var a =
 `
  .d8888b.  888                       888
@@ -33,7 +15,3 @@ Y88b  d88P Y88b.  Y88..88P 888 d88P         account, it is a scam and will give 
 See https://www.facebook.com/selfxss for more information.
 `;
 console.log(a);
-
-function subscribe(){
-  alert('not implemented!');
-}
