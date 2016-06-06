2603d917579a36ea67c8c685b7d6898210c32c4e
Shrimadhav U K <spechide@shrimadhavuk.me>
2016-06-06 10:27:54 +0530

    fail!

--- FrontEnd/js/functions.js
+++ FrontEnd/js/functions.js
@@ -1,22 +1,4 @@
 
-var a =
-`
-.d8888b.\ \ 888\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ 888\n
-d88P\ \ Y88b\ 888\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ 888\n
-Y88b.\ \ \ \ \ \ 888\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ 888\ \ \ \ This\ is\ a\ browser\ feature\ intended\ for\n
-"Y888b.\ \ \ 888888\ \ .d88b.\ \ 88888b.\ \ \ 888\ \ \ \ developers.\ If\ someone\ told\ you\ to\ copy\n
-\ \ \ "Y88b.\ 888\ \ \ \ d88""88b\ 888\ "88b\ \ 888\ \ \ \ and\ paste\ something\ here\ to\ enable\ a\n
-\ \ \ \ \ "888\ 888\ \ \ \ 888\ \ 888\ 888\ \ 888\ \ Y8P\ \ \ \ Facebook\ feature\ or\ "hack"\ someone's\n
-Y88b\ \ d88P\ Y88b.\ \ Y88..88P\ 888\ d88P\ \ \ \ \ \ \ \ \ account,\ it\ is\ a\ scam\ and\ will\ give\ them\n
-"Y8888P"\ \ \ "Y888\ \ "Y88P"\ \ 88888P"\ \ \ 888\ \ \ \ access\ to\ your\ Facebook\ account.\n
-\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ 888\n
-\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ 888\n
-\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ 888\n
-
-See https://www.facebook.com/selfxss for more information.
-`;
-console.log(a);
-
 function sendEMail(){
 
 		var url = "../BackEnd/send-mail.php";
