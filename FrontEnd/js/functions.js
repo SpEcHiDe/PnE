74571d4f8ad66b4df51573afc19fcace130b095f
Shrimadhav U K <spechide@shrimadhavuk.me>
2016-06-14 19:27:11 +0530

    unnecassary commit

--- FrontEnd/js/functions.js
+++ FrontEnd/js/functions.js
@@ -3,36 +3,42 @@ function countChar(val, lav){
 	var len = val.value.length;
 	var the_real_display = lav;
 	if(len > 150){
-		the_real_display.style.color = "red";
+		the_real_display.style.color = "#ff0000";
 	}
 	else{
-		the_real_display.style.color = "#fff";
+		the_real_display.style.color = "#ffffff";
 	}
 	the_real_display.innerHTML = len + "/150";
 }
 
-function sendEMail(){
+var sendData = function(type, URL, formData, callBack){
+  var xhr = new XMLHttpRequest();
+  xhr.open(type, URL, true);
+  xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded; charset=ISO-8859-1')
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      var response = xhr.responseText;
+      callBack(response);
+    }
+  };
+  if(type == "GET"){
+    xhr.send();
+  }
+  if(type == "POST"){
+    xhr.send(formData);
+  }
+};
 
+function sendEMail(){
 		var url = "../BackEnd/send-mail.php";
 
 		var from = document.getElementById('fromname').value + "<" + document.getElementById('fromemail').value + ">";
-		var to = "FROM NAME <fromname@fromdomain.tld>";
-		var sub = document.getElementById('sub').value;
+		var to = "TO NAME <toname@todomain.totld>";
+		var sub = "PnE subject";
 		var msg = document.getElementById('msg').value;
 		var formdata = "from="+from+"&to="+to+"&sub="+sub+"&msg="+msg+"";
-		console.log(formdata);
-
-		var xhttp = new XMLHttpRequest();
-		xhttp.open('GET',url+'?'+formdata,true);
-		xhttp.onreadystatechange = function() {
-			if (xhttp.readyState === 4) {
-				if (xhttp.status >= 200 && xhttp.status < 400) {
-					console.log(xhttp.responseText);
-					//document.getElementsByClassName('form')[0].innerHTML='successfully sent the electronic mail';
-				} else {
-					console.log(new Error('Response returned with non-OK status'));
-				}
-			}
-		};
-		xhttp.send();
+		
+		sendData("POST", url, formdata, function(r){
+			console.log(r);
+		});
 }
